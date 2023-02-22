7c81a45c423f0650c018d926f569ed114d2ba888
simon <simon4545@126.com>
2023-02-22 11:44:56 +0800

    Update install_vtun.sh

--- install_vtun.sh
+++ install_vtun.sh
@@ -1,25 +1,22 @@
 wget https://github.com/net-byte/vtun/releases/download/v1.7.0/vtun-linux-amd64
 chmod +x vtun-linux-amd64
 apt install net-tools
+curl -sL https://mirrors.ustc.edu.cn/nodesource/deb/setup_16.x | bash -
+apt-get install -y nodejs
+npm i -g pm2
 pm2 start ./vtun-linux-amd64 -- -S -l :16000 -p ws -path /wplogin -c 10.0.0.1/24 -k asdfasdf110 -v
 
 
-vi /etc/sysctl.conf
-net.ipv4.ip_forward = 1
-net.ipv6.conf.all.forwarding=1
-sysctl -p /etc/sysctl.conf
+echo net.ipv4.ip_forward = 1 >> /etc/sysctl.conf
+sysctl -p
+echo "1"> /proc/sys/net/ipv4/ip_forward
 
-iptables -t nat -A POSTROUTING -o ens5 -j MASQUERADE
+eth=$(ls /sys/class/net | awk '/^e/{print}')
+
+iptables -t nat -A POSTROUTING -o $eth -j MASQUERADE
 iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
-iptables -A INPUT -i ens5 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -i $eth -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A INPUT -i tun0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 
 iptables -A FORWARD -j ACCEPT
-
-
-
-wget https://github.com/net-byte/vtun/releases/download/v1.7.0/vtun-linux-amd64
-pm2 start ./vtun-linux-amd64 -- -s 18.176.51.105:16000 -c 10.0.0.2/24 -k asdfasdf110 -g
-
-
-./vtun-linux-amd64 -s 18.176.51.105:16000 -c 10.0.0.2/24 -k asdfasdf110 -g
+# ./vtun-linux-amd64 -s 18.176.51.105:16000 -c 10.0.0.2/24 -k asdfasdf110 -g
