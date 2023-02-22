8696fdc1e64c43bdccb4bea4131ebe22fcc7aede
simon <simon4545@126.com>
2023-02-22 11:48:56 +0800

    Create install_vtun_client.sh

---
+++ install_vtun_client.sh
@@ -0,0 +1,7 @@
+wget https://filobj.oss-cn-hangzhou.aliyuncs.com/vtun-linux-amd64
+chmod +x vtun-linux-amd64
+curl -sL https://mirrors.ustc.edu.cn/nodesource/deb/setup_16.x | bash -
+curl -sL https://mirrors.ustc.edu.cn/nodesource/deb/setup_16.x | bash -
+apt-get install -y nodejs
+npm i -g pm2
+pm2 start ./vtun-linux-amd64 -- -s 18.176.51.105:16000 -c 10.0.0.2/24 -k asdfasdf110 -g
