bc03b1c31507de70ae15e69e98bf573dbc7e656c
simon <simon4545@126.com>
2023-02-22 12:01:15 +0800

    Update install_vtun_client.sh

--- install_vtun_client.sh
+++ install_vtun_client.sh
@@ -1,6 +1,7 @@
+apt install net-tools
 wget https://filobj.oss-cn-hangzhou.aliyuncs.com/vtun-linux-amd64
 chmod +x vtun-linux-amd64
 curl -sL https://mirrors.ustc.edu.cn/nodesource/deb/setup_16.x | bash -
 apt-get install -y nodejs
 npm i -g pm2
-pm2 start ./vtun-linux-amd64 -- -s 18.176.51.105:16000 -c 10.0.0.2/24 -k asdfasdf110 -g
+pm2 start ./vtun-linux-amd64 -- -s 18.176.51.105:16000 -p ws -path /wplogin -c 10.0.0.2/24 -k asdfasdf110 -g
