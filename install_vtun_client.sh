5211222287a38856ac8be128a7d26ffb41978ca1
simon <simon4545@126.com>
2023-02-22 12:02:21 +0800

    Update install_vtun_client.sh

--- install_vtun_client.sh
+++ install_vtun_client.sh
@@ -4,4 +4,4 @@ chmod +x vtun-linux-amd64
 curl -sL https://mirrors.ustc.edu.cn/nodesource/deb/setup_16.x | bash -
 apt-get install -y nodejs
 npm i -g pm2
-pm2 start ./vtun-linux-amd64 -- -s 18.176.51.105:16000 -p ws -path /wplogin -c 10.0.0.2/24 -k asdfasdf110 -g
+pm2 start ./vtun-linux-amd64 -- -s 18.176.51.105:16000 -obfs -p ws -path /wplogin -c 10.0.0.2/24 -k asdfasdf110 -g
